9a85d37b67950572eef300bd5cc23609b620a803
sagar korde <sagargk1@gmailcom>
2019-10-17 13:12:59 +0530

    Hi there.....my 1st commit

---
+++ src/fgcvbhjnm/A.java
@@ -0,0 +1,15 @@
+package fgcvbhjnm;
+import static java.lang.Math.abs;
+
+public  class A {
+	
+public static void main(String[] args) {
+	
+	abs(-5);
+	Math.addExact(5, 10);
+	Math.subtractExact(4, 2);
+	Math.multiplyExact(5, 4);
+	
+}	
+
+}
---
+++ src/fgcvbhjnm/main.java
@@ -0,0 +1,14 @@
+package fgcvbhjnm;
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.MAX_VALUE);
+		
+		System.out.println(Integer.compare(5, 5));
+		System.out.println(Integer.compare(5, 6));
+		System.out.println(Integer.compare(6, 5));
+	}
+
+}
